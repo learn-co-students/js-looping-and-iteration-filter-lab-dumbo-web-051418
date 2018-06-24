af163e867b4956b3c4c4a4270b1b484029e6e962
prmoreira23 <prm.gredes@gmail.com>
2018-06-24 12:43:37 -0400

    Add functions to find elements in an array.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file
+function findMatching(arr, name){
+  const newArr = [];
+  for(const nome of arr){
+    if(name.toLowerCase() === nome.toLowerCase()){
+      newArr.push(nome);
+    }
+  }
+  return newArr;
+}
+
+function fuzzyMatch(arr, name){
+  const newArr = [];
+  for(const nome of arr){
+    if(name.toLowerCase() === nome.slice(0, name.length).toLowerCase()){
+      newArr.push(nome);
+    }
+  }
+  return newArr;
+}
+
+function matchName(arr, name){
+  const newArr = [];
+  for(const nome of arr){
+    if(name.toLowerCase() === nome.name.toLowerCase()){
+      newArr.push(nome);
+    }
+  }
+  return newArr;
+}
