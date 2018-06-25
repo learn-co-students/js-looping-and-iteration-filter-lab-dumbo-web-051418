d9b8c80b98698a110acdad74c1390b27daebae43
pablopawpaw <lpault29@gmail.com>
2018-06-25 12:29:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file
+function findMatching(arrayOfNames, person) {
+  return arrayOfNames.filter(function(name) { return name.toLowerCase() === person.toLowerCase() })
+};
+
+function fuzzyMatch(arrayOfNames, person) {
+  const result = arrayOfNames.filter(function(name) {
+    if (name.toLowerCase().startsWith(person.toLowerCase())) {
+      return name
+    }
+  })
+  return result
+};
+
+function matchName(arrayOfNames, person) {
+  const result = arrayOfNames.filter(function(element) {
+    if(element.name === person) {
+      return element
+    }
+  })
+  return result
+};
